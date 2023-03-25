015aba216ecc230ad514bd1fd95d5fac6431bf2f
Loise <loisewakonyo18@gmail.com>
2023-03-25 03:38:15 +0300

    addition of index,show and update to  powers route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :hero_powers, only: [:create]
-  resources :powers
+  resources :powers, only: [:index, :show, :update]
   resources :heros
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
