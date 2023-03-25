fd52429ef5d21462053f9d1937b99569c6d527b5
Loise <loisewakonyo18@gmail.com>
2023-03-25 04:14:59 +0300

    changes to powers controllers in that I added update

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -4,8 +4,19 @@ class PowersController < ApplicationController
         render json: power
     end
 
-    def  show
+    def  update
      power = Power.find(params[:id])
      render json: power
     end
+
+    def update
+      power = power.find_by(id: params[:id])
+       if power
+         power.update(power_params)
+         render json: power
+        else
+          render json: { error: "Power not found" }, status: :not_found
+        end
+    end
+
 end
